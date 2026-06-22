ef50beab754ed0b2657e6cc132d1c1481ee5a0b9
T28 <danie.castroc@duocuc.cl>
2026-06-22 10:47:52 -0400

    swagger

---
+++ Abrir_Swaggers.bat
@@ -0,0 +1,20 @@
+@echo off
+echo ==================================================
+echo      Abriendo Swaggers del Sistema VetNova...
+echo ==================================================
+
+start http://localhost:8081/swagger-ui.html
+start http://localhost:8092/swagger-ui.html
+start http://localhost:8083/swagger-ui.html
+start http://localhost:8084/swagger-ui.html
+start http://localhost:8085/swagger-ui.html
+start http://localhost:8086/swagger-ui.html
+start http://localhost:8087/swagger-ui.html
+start http://localhost:8088/swagger-ui.html
+start http://localhost:8089/swagger-ui.html
+start http://localhost:8090/swagger-ui.html
+
+echo.
+echo ¡Listo! Revisa las pestanas de tu navegador predeterminado.
+echo.
+pause
